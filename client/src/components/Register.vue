df917b0652bc40e7b0bc1f7c31f6d9a5cd1674ed
O.A.Ajayi <oaa64@kent.ac.uk>
2017-09-21 12:42:45 +0100

    setting up our registeration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    
+    <input type="email" name="email" v-model="email" placeholder="Enter Email">
+    <br>
+    <input type="password" name="password" v-model="password" placeholder="Enter Password">
+    <br>
+    <button v-on:click="register" type="submit">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ client/src/services/AuthenticationService.js
@@ -0,0 +1,10 @@
+import Api from '@/services/Api'
+
+export default {
+  register (credentials) { return Api().post('register', credentials) }
+}
+
+/* AuthenticationService.register({
+  email: 'test@test.com'
+  password: '1233456'
+}) */
